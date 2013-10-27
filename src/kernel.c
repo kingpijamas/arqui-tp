870f0425a3da866b7b061893916c74886840c881
kpjjpk <juanpablorey.10@gmail.com>
2013-10-27 16:15:46 -0300

    Changed the style of kernel.c; enabled the INT 80 driver

--- src/kernel.c
+++ src/kernel.c
@@ -10,38 +10,30 @@ int tickpos=640;
 
 /**********************************************
 kmain() 
-Punto de entrada de cóo C.
+Punto de entrada de codigo C.
 *************************************************/
-
 kmain() {
-
 	int i,num;
-
-/* Borra la pantalla. */ 
-
+ 
+	// Clears the screen
 	k_clear_screen();
 
 
-/* CARGA DE IDT CON LA RUTINA DE ATENCION DE IRQ0    */
-
+	// Carga de IDT con la rutina de atencion de IRQ0
 	setup_IDT_entry (&idt[0x08], 0x08, (dword)&_int_08_hand, ACS_INT, 0);
 
-/* CARGA DE IDT CON LA RUTINA DE ATENCION DE IRQ80    */
-	
-//	setup_IDT_entry (&idt[0x80], 0x80, (dword)&_int_80_hand, ACS_INT, 0);
+	setup_IDT_entry (&idt[0x80], 0x80, (dword)&_int_80_hand, ACS_INT, 0);
 
-/* Carga de IDTR    */
-
+	// Carga de IDTR
 	idtr.base = 0;  
-	idtr.base +=(dword) &idt;
+	idtr.base += (dword) &idt;
 	idtr.limit = sizeof(idt)-1;
 	
 	_lidt (&idtr);	
 
 	_Cli();
 
-/* Habilito interrupcion de timer tick*/
-
+	// Habilito interrupcion de timer tick
 	_mascaraPIC1(0xFC); /* Timer tick y teclado */
 	_mascaraPIC2(0xFF);
 
