35a43b2f4109b6b3a327689c60a63b0e530118a7
kpjjpk <juanpablorey.10@gmail.com>
2013-11-12 22:18:33 -0300

    Fixes extra ) that broke compilation

--- include/video.h
+++ include/video.h
@@ -6,7 +6,7 @@
 #define VIDEO_ADDRESS			0xb8000
 #define BASE_PORT				0x3D4
 
-#define VIDEO_BUFFER_SIZE		VIDEO_AREA(WIDTH,STD_DISPLAY_HEIGHT))
+#define VIDEO_BUFFER_SIZE		VIDEO_AREA(WIDTH,STD_DISPLAY_HEIGHT)
 
 //FIXME all these should be some sort of enum
 #define INVALID_DISPLAY 		-1
